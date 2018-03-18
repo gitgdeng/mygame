755595671ddc0d1d662d0dcbc1a83873bf225d0f
Daniel Deng <gang.deng@outlook.com>
2018-03-18 14:12:11 +0800

    refactor as module

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 def run_game():
     '''
@@ -23,16 +24,10 @@ def run_game():
     # 开始游戏主循环
     while True:
         # 监视键盘和鼠标事件
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        gf.check_events()
         
         # 每次循环都重回屏幕
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-
-        # 让最近绘制的屏幕可见
-        pygame.display.flip()
+        gf.update_screen(ai_settings, screen, ship)
 
 run_game()                
 
--- settings.py
+++ settings.py
@@ -6,4 +6,4 @@ class Settings():
 		# 屏幕设置
 		self.screen_width = 800
 		self.screen_height = 600
-		self.bg_color = (230, 230, 230)
+		self.bg_color = (120, 120, 230)
